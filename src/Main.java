4729d7c7378da45a060bdbc4f250aa8310c3e59c
Ramon Mattlov <ramon_mattlov@vulong-development.ru>
2022-07-30 00:51:13 +0300

    Создан класс Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
+        int appleSumCurrency = 100;
+        int appleCount = 5;
+        System.out.println("Вы должны заплатить за яблоки = " + appleCount*appleSumCurrency);
     }
 }
